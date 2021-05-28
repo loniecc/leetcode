e044ace384e86ff899b24b75fc0fee07959519ac
huzeming <huzeming@sensorsdata.cn>
2021-05-28 13:52:11 +0800

    [01]

--- 11.盛最多水的容器.java
+++ 11.盛最多水的容器.java
@@ -1,3 +1,4 @@
+
 /*
  * @lc app=leetcode.cn id=11 lang=java
  *
@@ -8,34 +9,20 @@
 class Solution {
 
     public int maxArea(int[] height) {
-        int res = 0;
-        int left = 0, right = height.length-1;
-
-        while(left < right){
-            boolean leftMin = false;
-            int minHeight = height[right];
-            if(height[left] < height[right]){
-                leftMin = true;
-                minHeight = height[left];
-            }
-            int tmpRes = (right - left) * minHeight;
-            if(tmpRes > res){
-                res = tmpRes;
-            }
-            if(leftMin){
-                left ++;
-            }else{
-                right --;
+        if (height == null) {
+            return 0;
+        }
+        int left = 0;
+        int right = height.length - 1;
+        int max = 0;
+        while (left < right) {
+            int area = (right - left) * (height[left] > height[right] ? height[right--] : height[left++]);
+            if (area > max) {
+                max = area;
             }
         }
-        return res;
-    }
-
-
-    public int getMinNumber(int a, int b){
-        return a>b?b:a;
+        return max;
     }
 
 }
 // @lc code=end
-
