4c69a3d1db41cb6d4e806871f6d47d7c670f9a57
huzeming <huzeming@sensorsdata.cn>
2021-06-05 13:58:44 +0800

    [review] 1,15

--- 1.两数之和.java
+++ 1.两数之和.java
@@ -11,10 +11,10 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> sub = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            if (sub.containsKey(target - nums[i])) {
-                return new int[] { i, sub.get(target - nums[i]) };
+            if (sub.containsKey(nums[i])) {
+                return new int[] { i, sub.get(nums[i]) };
             }
-            sub.put(nums[i], i);
+            sub.put(target - nums[i], i);
         }
         return new int[] {};
     }
--- 15.三数之和.java
+++ 15.三数之和.java
@@ -1,14 +1,78 @@
+
 /*
  * @lc app=leetcode.cn id=15 lang=java
  *
  * [15] 三数之和
  */
+import java.util.*;
 
 // @lc code=start
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums.length < 3) {
+            return result;
+        }
+        qucikSort(nums, 0, nums.length - 1);
+        System.out.println(Arrays.toString(nums));
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] > 0) {
+                return result;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int min = nums[i];
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                if (min + nums[left] + nums[right] == 0) {
+                    result.add(Arrays.asList(min, nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (nums[left] + nums[right] + min < 0) {
+                    left++;
+                } else
+                    right--;
+            }
 
+        }
+        return result;
     }
+
+    public void qucikSort(int[] nums, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int flag = nums[start];
+        int left = start, right = end;
+        while (left < right) {
+            while (left < right && nums[right] > flag) {
+                right--;
+            }
+
+            if (left < right) {
+                nums[left++] = nums[right];
+            }
+
+            while (left < right && nums[left] < flag) {
+                left++;
+            }
+
+            if (left < right) {
+                nums[right--] = nums[left];
+            }
+        }
+        nums[left] = flag;
+        qucikSort(nums, start, left - 1);
+        qucikSort(nums, left + 1, end);
+    }
+
 }
 // @lc code=end
-
